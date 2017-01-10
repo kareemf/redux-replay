8582fe5c5b05c23b9f799f1624c271cef9fc832a
Kareem Francis <francis.kareem+git@gmail.com>
2017-01-10 08:11:59 -0500

    WIP implementation of Firebase counter example

--- examples/firebase-transport/src/index.js
+++ examples/firebase-transport/src/index.js
@@ -1,20 +1,72 @@
+import uuid from 'uuid';
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
+
+import { createStore, applyMiddleware, compose } from 'redux';
+import createLogger from 'redux-logger';
+import createReplayer, { createFirebaseTransport } from 'redux-replay';
+
 import Counter from './components/Counter'
 import counter from './reducers'
+import setupFirebase from './setupFirebase'
 
-const store = createStore(counter)
-const rootEl = document.getElementById('root')
+// TODO: read from config
+const LOGGING_APP_ID = `firebase-counter`;
+const LOGGING_SESSION_ID = uuid.v4();
 
-const render = () => ReactDOM.render(
-  <Counter
-    value={store.getState()}
-    onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
-    onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
-  />,
-  rootEl
-)
+// TODO: read from config
+const firebaseConfig = require('firebaseConfig.json')
 
-render()
-store.subscribe(render)
+// TODO:
+// dbRef -> ref
+// renderApp -> render
+
+setupFirebase(firebaseConfig).then(({ ref, path }) => {
+  const { logPersister, logRetreaver } = createFirebaseTransport({
+    dbRef: ref,
+    path,
+    appId: LOGGING_APP_ID,
+    sessionId: LOGGING_SESSION_ID,
+  });
+
+  const logger = createLogger({
+    // log all actions except replayed actions
+    predicate: (getState, action) => !action.__replayed,
+    persister: logPersister
+  });
+
+  const rootReducer = (state, action) => {
+    if (action.type === `RESET_STATE`) {
+      return counter(undefined, action);
+    }
+
+    return counter(state, action);
+  };
+
+  const store = compose(
+    applyMiddleware(logger)
+  )(createStore)(rootReducer);
+
+  const rootEl = document.getElementById('root')
+
+  const render = () => ReactDOM.render(
+    <Counter
+      value={store.getState()}
+      onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+      onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+    />,
+    rootEl
+  )
+
+  const stateResetAction = { type: `RESET_STATE` };
+  const replaySession = createReplayer({
+    store,
+    renderApp: render,
+    stateResetAction,
+    logRetreaver,
+  });
+
+  render()
+  store.subscribe(render)
+  window.replaySession = replaySession;
+});
---
+++ examples/firebase-transport/src/setupFirebase.js
@@ -0,0 +1,34 @@
+import * as firebase from "firebase";
+
+const path = `log-entries`;
+const firebaseRefKey = `/sessions`;
+
+const setupFirebase = (config) => {
+  const authPromise = new Promise((resolve, reject) => {
+    firebase.initializeApp(config);
+
+    firebase.auth().signInAnonymously().catch((error) => {
+      reject(error);
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      let ref;
+      if (user) {
+        // User is signed in.
+        ref = firebase
+          .database()
+          .ref(firebaseRefKey);
+
+        resolve({ ref, path });
+      } else {
+        // User is signed out.
+        ref = null;
+        reject({ ref, path });
+      }
+    });
+  });
+
+  return authPromise;
+};
+
+export default setupFirebase;
